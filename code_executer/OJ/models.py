a65d2b83828c784393767829fd7df222feed29a8
Vishal <vishal@brightmoney.co>
2021-02-02 23:38:03 +0530

    project setup

---
+++ code_executer/OJ/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from enum import Enum
+# Create your models here.
+
+class ChoiceEnum(Enum):
+
+    @classmethod
+    def choices(cls):
+        return [(x.name,x.value) for x in cls] 
+
+class SubmissionStatus(ChoiceEnum):
+    NT = 'NOT TESTED'
+    RE = 'RUNTIME ERROR'
+    TLE = 'TIME LIMIT EXCEEDED'
+    AC = 'ACCEPTED'
+    WA = 'WRONG ANSWER'
+    CE = 'COMPILE ERROR'
+
+class Language(ChoiceEnum):
+    C = 'GNU C'
+    CPP = 'GNU C++'
+    JAVA = 'JAVA'
+    PYTHON3 = 'PYTHON3'
+    PYTHON2 = 'PYTHON2'
+
+class Submission(models.Model):
+    submitter = models.CharField(max_length = 50, null=True,blank=True)
+    status = models.CharField(max_length = 10, default = "NT", choices = SubmissionStatus.choices())
+    language = models.CharField(max_length = 10, default = "C", choices = Language.choices())
+    code = models.TextField(default="")
+    created = models.DateTimeField(auto_now_add=True)
+    private = models.BooleanField(default = True)
